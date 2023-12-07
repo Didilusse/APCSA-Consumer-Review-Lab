1eed83799de57b8ab1d2df652d51c9d64ad3f8cd
Didilusse <77905666+Didilusse@users.noreply.github.com>
2023-12-07 00:09:23 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) 
+    {
+        // Activity 1: Call the sentimentVal method in Review with a word 
+        //     like "terrible" and print out the result
+        
+        System.out.println(Review.totalSentiment("simpleReview.txt"));
+        System.out.println(Review.starRating("simpleReview.txt"));
+        System.out.println(Review.fakeReview("simpleReview.txt"));
+    
+    }
+}
